5602f3c312e3db213f412c415754c6a4b0d43c90
Tushar <tusharbajaj10@gmail.com>
2025-02-19 20:36:52 +0530

    major changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,27 +1,30 @@
 import React from 'react'
-import logo from '/public/Logo.png'
+// import logo from '/public/Logo.png'
 import rocket from '/public/RocketLaunch.png'
 import { Button } from './ui/button'
+import { ClapIcon } from './ui/clap'
 
 const Navbar = () => {
     return (
         <div className='w-full flex items-center justify-between px-6 py-4'>
             <div className='flex items-center'>
-                <div>
-                    <img src={logo.src} alt="" />
-                </div>
+                <ClapIcon />
                 <div className='text-xl text-[#2D2D2D] hidden md:block'>
                     <span className='font-bold'>Landing</span>
                     <span>Page</span>
                 </div>
             </div>
             <div className='gap-10 hidden sm:flex'>
-                <div className='flex items-center font-semibold gap-4 text-[#009379]'>
-                    <div>
+                <div className='flex items-center gap-4 text-[#009379]'>
+                    <div className='cursor-pointer relative group '>
                         Contact
+                        <span className='h-[0.75px] w-full bottom-0 right-0 bg-[#009379] absolute group-hover:w-0 duration-300'></span>
+                        <span className='h-[0.75px] w-0 bottom-0 left-0 bg-[#009379] absolute group-hover:w-full duration-300 delay-100'></span>
                     </div>
-                    <div>
+                    <div className='cursor-pointer relative group '>
                         How it works
+                        <span className='h-[0.75px] w-full bottom-0 right-0 bg-[#009379] absolute group-hover:w-0 duration-300'></span>
+                        <span className='h-[0.75px] w-0 bottom-0 left-0 bg-[#009379] absolute group-hover:w-full duration-300 delay-100'></span>
                     </div>
                 </div>
                 <div className=''>
